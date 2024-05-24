a1fe2ce3de68889a03af31039dd6f431493022a9
Maxisthatyou <158555539+Maxisthatyou@users.noreply.github.com>
2024-05-24 13:20:09 -0700

    05/24/2024

--- assignment.py
+++ assignment.py
@@ -10,6 +10,7 @@ import one
 import game
 import 起飞速度
 import units
+import volume
 def title(a = "*", b = " "):
     print(f"{a * 12} THE CALCULATOR {a*12} \n {b * 12} BY: 芝士666 {b*12}")
     return None
@@ -25,24 +26,26 @@ def main():
     title()
     instructions()
     while True:
-        x = input("Please select what you'd like to calculate... \n[1] Rockets \n[2] Fuel Consumption \n[3] Gravity of Object \n[4] Velocity of Rotate (Takeoff) \n[5] Fuel Requirement \n[6] Convert Units \n[7] Play Roulette \n[8] Play Keep-Under-10 \n[9] Play Guess the Word \n> ").strip()
+        x = input("Please select what you'd like to calculate... \n[1] SA and Volume \n[2] Rockets \n[3] Fuel Consumption \n[4] Gravity of Object \n[5] Velocity of Rotate (Takeoff) \n[6] Fuel Requirement \n[7] Convert Units \n[8] Play Roulette \n[9] Play Keep-Under-10 \n[10] Play Guess the Word \n> ").strip()
         if x == "1":
-            rockets.rockets()
+            volume.rad()
         elif x == "2":
-            燃油消耗.fuel()
+            rockets.rockets()
         elif x == "3":
-            牛逼1.nb1()
+            燃油消耗.fuel()
         elif x == "4":
-            起飞速度.VR()
+            牛逼1.nb1()
         elif x == "5":
-            one.calc()
+            起飞速度.VR()
         elif x == "6":
-            units.unit()
+            one.calc()
         elif x == "7":
-            roulette.roulette()
+            units.unit()
         elif x == "8":
-            mathgame.game()
+            roulette.roulette()
         elif x == "9":
+            mathgame.game()
+        elif x == "10":
             game.chooseword()
         else:
             print("Invalid input, please try again")
---
+++ volume.py
@@ -0,0 +1,13 @@
+import math
+def rad():
+    r=input("The radius of the ball:")
+    if float(r) < 0:
+        print("Invalid input")
+    else:
+        r = float(r)
+        s= math.pi*4*r*r
+        s1=round(s,2)
+        print(f"The surface area is {s1}")
+        v=(4/3)*math.pi*(r**3)
+        v1=round(v,2)
+        print(f"The volume is {v1}")
